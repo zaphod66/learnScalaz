1f5ddc291dc24511b8c794db4b74bdeaaf0e2c71
Norbert Scheller <norbert@riskident.com>
2016-03-04 10:31:09 +0100

    day04

--- src/main/scala/com/myspace/learning/day04.scala
+++ src/main/scala/com/myspace/learning/day04.scala
@@ -2,19 +2,50 @@ package com.myspace.learning
 
 import scalaz._, Scalaz._
 
+import scala.language.implicitConversions
+
 object day04 extends App {
 
   println("learning Scalaz day04")
+  // Functor - for things that can be mapped over
 
-  // Breaking the functor law
-  sealed trait COption[+A] {}
-  case class CSome[A](counter: Int, a: A) extends COption[A]
-  case object CNone extends COption[Nothing]
+  // Functor laws
+  // Identity - mapping identity yields the input
+  List(1, 2, 3) map identity assert_=== List(1, 2, 3)
 
-  implicit def coptionEqual[A]: Equal[COption[A]] = Equal.equalA
-  implicit val coptionFunctor = new Functor[COption] {
-    def map[A, B](fa: COption[A])(f: A => B): COption[B] = fa match { case CNone => CNone
-    case CSome(c, a) => CSome(c + 1, f(a))
+  // Associativity
+  (List(1, 2, 3) map {{(_: Int) * 3} map {(_: Int) + 1}}) assert_=== (List(1, 2, 3) map {(_: Int) * 3} map {(_: Int) + 1})
+
+  // Option as Monoid
+  implicit def optionMonoid[A: Semigroup]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    def zero: Option[A] = None
+
+    def append(f1: Option[A], f2: => Option[A]) = (f1, f2) match {
+      case (Some(a1), Some(a2)) => Some(Semigroup[A].append(a1, a2))
+      case (Some(a1), None)     => f1
+      case (None, Some(a2))     => f2
+      case (None, None)         => None
     }
   }
+
+  val o01 = (none: Option[String]) |+| "andy".some    // Some("andy")
+  val o02 = (Ordering.LT: Ordering).some |+| none     // Some(LT)
+
+  // mappend should take the first one if both are Some -> First tag
+  val o03 = Tags.First('a'.some) |+| Tags.First('b'.some)           // Some('a')
+  val o04 = Tags.First(none: Option[Char]) |+| Tags.First('b'.some) // Some('b')
+  val o05 = Tags.First('a'.some) |+| Tags.First(none: Option[Char]) // Some('a')
+
+  // mappend should take the second one if both are Some -> Last tag
+  val o06 = Tags.Last('a'.some) |+| Tags.Last('b'.some)             // Some('b')
+  val o07 = Tags.Last(none: Option[Char]) |+| Tags.Last('b'.some)   // Some('b')
+  val o08 = Tags.Last('a'.some) |+| Tags.Last(none: Option[Char])   // Some('a')
+
+  // Foldable - for things that can be folded up
+  val f01 = List(1, 2, 3).foldRight (1) {_ * _}
+  val f02 = 9.some.foldLeft(2) {_ + _}
+
+  // for elements that are Monoids one can use foldMap to first map to a Monoid
+  val f03 = List(1, 2, 3) foldMap identity
+  val f04 = List(true, false, true, true) foldMap Tags.Disjunction.apply
 }
--- src/test/scala/com/myspace/learning/day04Test.scala
+++ src/test/scala/com/myspace/learning/day04Test.scala
@@ -3,11 +3,29 @@ package com.myspace.learning
 import scala.language.implicitConversions
 
 import scalaz._, Scalaz._
-import org.scalacheck.{Gen, Arbitrary}
 
-import com.myspace.learning.day04._
+import scalaz.scalacheck.ScalaCheckBinding._
+import scalaz.scalacheck.ScalazProperties._
+import org.scalacheck.Arbitrary
 
-class day04Test {
+object day04Test extends App {
+  // Functor Laws
+  println("==========\nFunctor List")
+  functor.laws[List].check
 
-//implicit def COptionArb[A](implicit arb: Arbitrary[A]): Arbitrary[COption[A]] = arb map { a => CSome(0, a): COption[A] }
+  // Breaking the functor law
+  sealed trait COption[+A] {}
+  case class CSome[A](counter: Int, a: A) extends COption[A]
+  case object CNone extends COption[Nothing]
+
+  implicit def coptionEqual[A]: Equal[COption[A]] = Equal.equalA
+  implicit val coptionFunctor = new Functor[COption] {
+    def map[A, B](fa: COption[A])(f: A => B): COption[B] = fa match { case CNone => CNone
+    case CSome(c, a) => CSome(c + 1, f(a))
+    }
+  }
+
+  implicit def COptionArb[A](implicit arb: Arbitrary[A]): Arbitrary[COption[A]] = arb map { a => CSome(0, a): COption[A] }
+  println("==========\nFunctor COption")
+  functor.laws[COption].check
 }
