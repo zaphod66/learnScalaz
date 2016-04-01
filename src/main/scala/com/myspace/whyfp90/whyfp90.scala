2897addbdd875e2fd716e49b75463a83a743faec
Norbert Scheller <norbert@riskident.com>
2016-04-01 10:05:28 +0200

    minimal changes

--- src/main/scala/com/myspace/whyfp90/whyfp90.scala
+++ src/main/scala/com/myspace/whyfp90/whyfp90.scala
@@ -10,7 +10,7 @@ object Whyfp90 extends App {
   def repeat(f: Double => Double)(a: Double): Stream[Double] = a #:: repeat(f)(f(a))
 
   def s1(n: Double) = repeat(squareRoot(n))(n)
-  val l1 = s1(2).take(5).toList
+  val l1 = s1(2).take(10).toList
 
   println(l1)
 
@@ -21,7 +21,9 @@ object Whyfp90 extends App {
 
   val d1 = within(0.0001)(repeat(next(2) _)(2)) // Double = 1.4142135623746899
 
-  def sqrt(eps: Double)(n: Double) = within(eps)(repeat(next(n) _)(n))
+  def witSqrt(eps: Double)(n: Double) = within(eps)(repeat(next(n) _)(n))
+
+  def wSqrt = witSqrt(0.0000001) _
 
   def relative(eps: Double)(s: Stream[Double]): Double = s match {
     case a #:: b #:: ss if math.abs((a / b) - 1) <= eps => b
@@ -29,4 +31,8 @@ object Whyfp90 extends App {
   }
 
   def relSqrt(eps: Double)(n: Double) = relative(eps)(repeat(next(n) _)(n))
+
+  def rSqrt = relSqrt(0.0000001) _
+
+  println(s"sqrt(2) = ${ wSqrt(2) }, ${ rSqrt(2) }")
 }
