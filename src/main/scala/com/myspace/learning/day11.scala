0a3576efa6367e4a7f843624c06cfc08c16b1d06
Norbert Scheller <norbert@riskident.com>
2016-04-13 10:01:44 +0200

    1st day11

--- src/main/scala/com/myspace/learning/day11.scala
+++ src/main/scala/com/myspace/learning/day11.scala
@@ -1,5 +1,18 @@
 package com.myspace.learning
 
-object day11 {
+import scalaz._, Scalaz._
 
+object day11 {
+  case class Point(x: Double, y: Double)
+  case class Color(r: Byte, g: Byte, b: Byte)
+  case class Turtle(position: Point, heading: Double, color: Color)
+
+  val t1 = Turtle(Point(2.0, 3.0), 0.0, Color(255.toByte, 255.toByte, 255.toByte))
+
+  val turtlePosition = Lens.lensu[Turtle, Point] ((a, value) => a.copy(position = value), _.position)
+  val positionX = Lens.lensu[Point, Double] ((a, value) => a.copy(x = value), _.x)
+  val positionY = Lens.lensu[Point, Double] ((a, value) => a.copy(y = value), _.y)
+
+  val turtleX = turtlePosition >=> positionX
+  val turtleY = turtlePosition >=> positionY
 }
