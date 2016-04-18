8c1943da1a765a84645be7e18b82cfbe8d93e0f8
Norbert Scheller <norbert@riskident.com>
2016-04-18 13:49:43 +0200

    refactor test

--- src/test/scala/com/myspace/learning/day11Test.scala
+++ src/test/scala/com/myspace/learning/day11Test.scala
@@ -3,8 +3,7 @@ package com.myspace.learning
 import com.myspace.learning.day11._
 
 import scala.language.implicitConversions
-import scalaz._
-import Scalaz._
+import scalaz._, Scalaz._
 import scalaz.scalacheck.ScalaCheckBinding._
 import scalaz.scalacheck.ScalazProperties._
 import org.scalacheck.Arbitrary
@@ -12,10 +11,24 @@ import org.scalacheck.Arbitrary
 object day11Test extends App {
   implicit def turtleEqual: Equal[Turtle] = Equal.equalA
 
-  implicit def TurtleArb(implicit arb: Arbitrary[Double]): Arbitrary[Turtle] = arb map { a =>
-    Turtle(Point(a, a), a, Color(1.toByte, 1.toByte, 1.toByte))
+  implicit def PointArb(implicit arb: Arbitrary[(Double, Double)]): Arbitrary[Point] = arb map { case (x, y) =>
+    Point(x, y)
   }
 
-  val laws = lens.laws[Turtle, Double](turtleX)
-  laws.check
+  implicit def ColorArb(implicit arb: Arbitrary[(Byte, Byte, Byte)]): Arbitrary[Color] = arb map { case (r, g, b) =>
+    Color(r, g, b)
+  }
+
+  implicit def TurtleArb(implicit arb: Arbitrary[(Point, Double, Color)]): Arbitrary[Turtle] = arb map { case (p, h, c) =>
+    Turtle(p, h, c)
+  }
+
+  val lawsTurtleX = lens.laws[Turtle, Double](turtleX)
+  val lawsTurtleY = lens.laws[Turtle, Double](turtleY)
+  val lawsHeading = lens.laws[Turtle, Double](turtleHeading)
+
+  lawsTurtleX.check
+  lawsTurtleY.check
+  lawsHeading.check
+
 }
