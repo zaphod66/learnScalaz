1fd2ec4199f6ff76b1cbf1539e89ad9c54ecce25
Norbert Scheller <norbert@riskident.com>
2016-04-20 17:56:12 +0200

    day16 - memo

--- src/main/scala/com/myspace/learning/day16.scala
+++ src/main/scala/com/myspace/learning/day16.scala
@@ -1,5 +1,38 @@
 package com.myspace.learning
 
-object day16 {
+import scalaz._, Scalaz._
 
+object day16 extends App {
+
+  // Memo
+
+  def slowFib: Int => Int = {
+    case 0 => 0
+    case 1 => 1
+    case n => slowFib(n - 2) + slowFib(n - 1)
+  }
+
+  val f1 = logTime { slowFib(30) }
+  val f2 = logTime { slowFib(40) }
+  val f3 = logTime { slowFib(45) }
+
+  val memoFib: Int => Int = Memo.mutableHashMapMemo {
+    case 0 => 0
+    case 1 => 1
+    case n => memoFib(n - 2) + memoFib(n - 1)
+  }
+
+  val f4 = logTime { memoFib(30) }
+  val f5 = logTime { memoFib(40) }
+  val f6 = logTime { memoFib(45) }
+
+  def logTime[T](e: => T): T = {
+    val start = System.currentTimeMillis()
+    val r = e
+    val stop  = System.currentTimeMillis()
+
+    println(s"took ${stop - start} msec")
+
+    r
+  }
 }
