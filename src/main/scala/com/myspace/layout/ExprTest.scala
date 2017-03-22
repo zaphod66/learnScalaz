15f553b1c6e4e6191127f572110827e192a0ba3b
Norbert Scheller <norbert@riskident.com>
2017-03-22 15:31:14 +0100

    layout - ExprFormatter

---
+++ src/main/scala/com/myspace/layout/ExprTest.scala
@@ -0,0 +1,18 @@
+package com.myspace.layout
+
+object ExprTest extends App {
+  val f = new ExprFormatter
+
+  val e1 = BinOp("*", BinOp("/", Number(1), Number(2)),
+                      BinOp("+", Var("x"), Number(1)))
+
+  val e2 = BinOp("+", BinOp("/", Var("x"), Number(2)),
+                      BinOp("/", Number(1.5), Var("x")))
+  val e3 = BinOp("/", e1, e2)
+
+  val e4 = BinOp("/", BinOp("/", Number(1), Number(2)), Number(3))
+
+  def show(e: Expr): Unit = println(f.format(e) + "\n\n")
+
+  List(e1, e2, e3, e4) foreach show
+}
