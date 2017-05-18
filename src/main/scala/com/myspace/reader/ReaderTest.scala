69ff5946c6c0573725570c204cdf77d69fe25d8f
Norbert Scheller <norbert@riskident.com>
2017-05-18 15:09:57 +0200

    readerInjection

--- src/main/scala/com/myspace/reader/ReaderTest.scala
+++ src/main/scala/com/myspace/reader/ReaderTest.scala
@@ -17,26 +17,21 @@ object MyStuff {
 
 }
 
-case class RichFunction1[R, A](r: R => A) {
+case class Function1Monad[R, A](r: R => A) {
+  def pure(a: => A): R => A = _ => a
   def map[B](f: A => B): R => B = r andThen f
   def flatMap[B](f: A => R => B): R => B = a => f(r(a))(a)
 }
 
-object RichFunction1 {
+object Function1Monad {
   import scala.language.implicitConversions
 
-  implicit def toRichFunction1[A, B](f: A => B): RichFunction1[A, B] = RichFunction1(f)
+  implicit def toRichFunction1[A, B](f: A => B): Function1Monad[A, B] = Function1Monad(f)
 }
 
 object MyModel {
 
   case class Account(no: String, name: String, balance: Long)
-
-  sealed trait AccountRepository {
-    def store(account: Account): Option[Account]
-    def query(no: String): Option[Account]
-    def delete(no: String): Unit
-  }
 
 //  def doSomething(id: String, repo: AccountRepository): Option[Account] = ???
 //  def cdoSomething(id: String): AccountRepository => Option[Account] = ???
@@ -60,6 +55,7 @@ object MyService {
 object Function1Service {
   import MyService._
   import MyModel._
+  import Repository.AccountRepository
 
   trait F1AccountService extends AccountService[Function1, AccountRepository, Option[Account]]
 }
@@ -67,10 +63,10 @@ object Function1Service {
 object Function1Injection {
   import MyModel._
   import Function1Service._
-  import RichFunction1._
+  import Function1Monad._
 
   object AccountService extends F1AccountService {
-    override def open(no: String, name: String): AccountRepository => Option[Account] = { repo =>
+    override def open(no: String, name: String) = { repo =>
       repo.query(no) match {
         case Some(_) => None
         case None    =>
@@ -80,7 +76,7 @@ object Function1Injection {
       }
     }
 
-    override def close(no: String): AccountRepository => Option[Account] = { repo =>
+    override def close(no: String) = { repo =>
       repo.query(no) match {
         case Some(acc) =>
           repo.delete(no)
@@ -89,7 +85,7 @@ object Function1Injection {
       }
     }
 
-    override def credit(no: String, amount: Long): AccountRepository => Option[Account] = { repo =>
+    override def credit(no: String, amount: Long) = { repo =>
       repo.query(no) match {
         case Some(acc) =>
           val newAcc = acc.copy(balance = acc.balance + amount)
@@ -100,7 +96,7 @@ object Function1Injection {
       }
     }
 
-    override def debit(no: String, amount: Long): AccountRepository => Option[Account] = { repo =>
+    override def debit(no: String, amount: Long) = { repo =>
       repo.query(no) match {
         case Some(acc) =>
           val newAcc = acc.copy(balance = acc.balance - amount)
@@ -125,22 +121,27 @@ object Function1Injection {
   }
 }
 
+object ReaderService {
+  import MyService._
+  import MyModel._
+  import MyStuff._
+  import Repository.AccountRepository
+
+  trait ReaderAccountService extends AccountService[Reader, AccountRepository, Option[Account]]
+}
+
+
 object ReaderInjection {
 
   import MyStuff._
   import MyModel._
+  import ReaderService._
+  import Repository.AccountRepository
 
-  trait AccountService {
-    def open(no: String, name: String): Reader[AccountRepository, Option[Account]]
-    def close(no: String): Reader[AccountRepository, Option[Account]]
-    def credit(no: String, amount: Long): Reader[AccountRepository, Option[Account]]
-    def debit(no: String, amount: Long): Reader[AccountRepository, Option[Account]]
-  }
-
-  object AccountService extends AccountService {
+  trait AccountService extends ReaderAccountService {
     override def open(no: String, name: String) = Reader { (repo: AccountRepository) =>
       repo.query(no) match {
-        case Some(_) => None
+        case Some(_) => { println(s"Account with $no already exists."); None }
         case None =>
           repo.store(Account(no, name, balance = 0))
           Some(Account(no, name, 0))
@@ -204,9 +205,71 @@ object ReaderInjection {
   }
 }
 
+object Repository {
+  import MyModel._
+
+  sealed trait AccountRepository {
+    def store(account: Account): Option[Account]
+    def query(no: String): Option[Account]
+    def delete(no: String): Option[Account]
+  }
+
+  trait AccountRepositoryInMemory extends AccountRepository {
+    import collection.mutable.{ Map => MMap }
+
+    lazy val repo = MMap.empty[String, Account]
+
+    override def store(account: Account): Option[Account] = {
+      repo += ((account.no, account))
+
+      Some(account)
+    }
+
+    override def query(no: String): Option[Account] = repo.get(no)
+
+    override def delete(no: String): Option[Account] = repo.remove(no)
+  }
+}
+
 object ReaderTest extends App {
-  import MyModel._, ReaderInjection._
-//  object ReaderInjectionApp extends AccountService {
-//
-//  }
+  import ReaderInjection._
+  object ReaderInjectionApp extends AccountService {
+
+    object Repo extends Repository.AccountRepositoryInMemory
+
+    val program = for {
+      _ <- open("a-123", "John Doe")
+      _ <- open("a-124", "Eva Luator")
+      _ <- open("a-125", "Mike Hammer")
+
+      _ <- credit("a-123", 10000)
+      _ <- credit("a-124",  1000)
+      _ <- credit("a-125",   100)
+
+      _ <- chain("a-123", "a-124", "a-125", 500)
+    } yield ()
+
+    val command = for {
+      _ <- transfer("a-124", "a-123", 200)
+      _ <- transfer("a-125", "a-124", 100)
+      _ <- chain("a-123", "a-124", "a-125", 500)
+    } yield ()
+
+    val delete = close("a-123")
+
+    program.run(Repo)
+    command.run(Repo)
+    command.run(Repo)
+    command.run(Repo)
+
+    Repo.repo foreach println
+
+    println("=========")
+
+    delete.run(Repo)
+
+    Repo.repo foreach println
+  }
+
+  ReaderInjectionApp
 }
