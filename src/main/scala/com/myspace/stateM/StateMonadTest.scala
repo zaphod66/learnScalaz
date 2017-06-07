00b1ee369c5d7b090f53e9d0f2d6ba9c0431627c
Norbert Scheller <norbert@riskident.com>
2017-06-07 12:24:53 +0200

    master - StateMonadTest new numberMy

--- src/main/scala/com/myspace/stateM/StateMonadTest.scala
+++ src/main/scala/com/myspace/stateM/StateMonadTest.scala
@@ -5,13 +5,22 @@ import scalaz._, Scalaz._
 object StateMonadTest extends App {
   sealed abstract class Tree[A] {
 
-    def number(seed: Int): (Tree[(A, Int)], Int) = this match {
-      case Leaf(a) => (Leaf(a, seed), seed + 1)
+    def number(seed: Int): (Int, Tree[(A, Int)]) = this match {
+      case Leaf(a) => (seed + 1, Leaf(a, seed))
       case Branch(left, right) => left number seed match {
-        case (l, ls) => right number ls match {
-          case (r, rs) => (Branch(l ,r), rs)
+        case (l, ls) => right number l match {
+          case (r, rs) => (r, Branch(ls ,rs))
         }
       }
+    }
+
+    def numberMy(seed: Int): (Int, Tree[(A, Int)]) = this match {
+      case Leaf(a)      => (seed + 1, Leaf(a, seed))
+      case Branch(l, r) =>
+        val (sl, ln) = l numberMy seed
+        val (sr, rn) = r numberMy sl
+
+        (sr, Branch(ln, rn))
     }
 
     def numberSM: State[Int, Tree[(A, Int)]] = this match {
@@ -37,13 +46,15 @@ object StateMonadTest extends App {
   final case class Leaf[A](a: A) extends Tree[A]
   final case class Branch[A](l: Tree[A], r: Tree[A]) extends Tree[A]
 
-  val tree1 = Branch(Leaf("one"), Branch(Leaf("two"), Leaf("three")))
+  val tree1 = Branch(Branch(Leaf("one"), Branch(Leaf("two"), Leaf("three"))), Leaf("four"))
   val treel1 = tree1.number(1)
   val treel2 = tree1.numberSM(1)
   val treel3 = tree1.numberSA(1)
+  val treel4 = tree1.numberMy(1)
 
   println(s"Tree1:  $tree1")
   println(s"Treel1: $treel1")
   println(s"Treel2: $treel2")
   println(s"Treel3: $treel3")
+  println(s"Treel4: $treel4")
 }
