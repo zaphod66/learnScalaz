4adf292c85b0a24d6e75a1e42139df9fd901cec8
Norbert Scheller <norbert@riskident.com>
2018-05-14 20:01:03 +0200

    hlist - some tries to fix things

--- src/main/scala/com/myspace/HLists/HListTest.scala
+++ src/main/scala/com/myspace/HLists/HListTest.scala
@@ -11,22 +11,40 @@ object MyImpl {
   trait LowerPriority {
     implicit def contains2[V,
                            H,
-                           T <: HList,
-                           L <: HCons[H, T]]
+                           T <: HCons[H, T],
+                           L <: HCons[V, T]]
       (implicit ev: Contains[V, T]): Contains[V, L] = null
   }
 
   object Contains extends LowerPriority {
     implicit def contains1[H, L <: HCons[H, _]]: Contains[H, L] = null
   }
+
+  type X = String
+  type Y = Int
+  type Z = Double
+  //type MyList = X :: Y :: Z :: HNil.type
+  type MyList = HCons[X, HCons[Y, HCons[Z, HNil.type]]]
+
+  val s = implicitly[Contains[String, MyList]]
 }
 
 
 object HListTest {
+  import MyImpl._
+
   // value level contains
   def contains[T](list: List[T], value: T): Boolean =
     list match {
       case Nil    => false
       case h :: t => h == value || contains(t, value)
     }
+
+//  type X = String
+//  type Y = Int
+//  type Z = Double
+////type MyList = X :: Y :: Z :: HNil.type
+//  type MyList = HCons[X, HCons[Y, HCons[Z, HNil.type]]]
+//
+//  val s = implicitly[Contains[String, MyList]]
 }
