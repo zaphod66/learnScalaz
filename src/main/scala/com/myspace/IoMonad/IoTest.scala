a212945817cfcc0e0415cd17a420739c7d80250c
Norbert Scheller <norbert@riskident.com>
2018-08-17 09:33:01 +0200

    IOMonad - FP to the Max 01

--- src/main/scala/com/myspace/IoMonad/IoTest.scala
+++ src/main/scala/com/myspace/IoMonad/IoTest.scala
@@ -2,12 +2,14 @@ package com.myspace.IoMonad
 
 import scalaz._
 
+import scala.util.Try
+
 sealed trait IO[A] { self =>
   def run: A
 
   def map[B](f: A => B): IO[B] = new IO[B] { def run = f(self.run) }
   def flatMap[B](f: A => IO[B]): IO[B] = new IO[B] { def run = f(self.run).run }
-
+  def withFilter(p: A => Boolean): IO[A] = ???
   def ++(io: IO[Unit]): IO[Unit] = new IO[Unit] {
     def run: Unit = { self.run; io.run }
   }
@@ -42,10 +44,17 @@ object IoTest extends App {
     c2.run
   }
 
-  object Converter {
-    def fahrenheitToCelsius(f: Double): Double = (f - 32) * 5.0/9.0
+  object IOUtil {
     def ReadLine: IO[String] = IO { scala.io.StdIn.readLine() }
     def PrintLine(s: String): IO[Unit]  = IO { println(s) }
+  }
+
+  object Converter {
+    import IOUtil._
+
+    def fahrenheitToCelsius(f: Double): Double = (f - 32) * 5.0/9.0
+//    def ReadLine: IO[String] = IO { scala.io.StdIn.readLine() }
+//    def PrintLine(s: String): IO[Unit]  = IO { println(s) }
 
     def converter: IO[Unit] = for {
       _ <- PrintLine("Enter temp in Fahrenheit: ")
@@ -54,7 +63,46 @@ object IoTest extends App {
     } yield ()
   }
 
-  FirstStep
+  object GuessGame {
+    import IOUtil._
 
-  Converter.converter.run
+    def nextInt(upper: Int): IO[Int] = IO { scala.util.Random.nextInt(upper) }
+
+    def parseInt(s: String): Option[Int] = Try(s.toInt).toOption
+
+    def checkContinue(name: String): IO[Boolean] =
+      for {
+        _     <- PrintLine(s"Do you want to continue, $name?")
+        input <- ReadLine
+        cont  <- input match {
+            case "y" => IO { true  }
+            case "n" => IO { false }
+            case _ => checkContinue(name)
+          }
+        } yield cont
+
+    def gameLoop(name: String): IO[Unit] =
+      for {
+        num   <- nextInt(5).map(_ + 1)
+        _     <- PrintLine(s"Dear $name, please guess a number from 1 to 5!")
+        input <- ReadLine
+        _     <- parseInt(input).fold(
+                   PrintLine("You did not enter a number.")
+                 )(guess =>
+                   if (guess == num) PrintLine(s"You guessed right!")
+                   else PrintLine(s"You guessed wrong, $name! The number was $num")
+                 )
+        cont  <- checkContinue(name)
+        _     <- if (cont) gameLoop(name) else IO{ () }
+      } yield ()
+
+    def game = gameLoop("Zaphod")
+
+  }
+
+//  FirstStep
+//
+//  Converter.converter.run
+
+  GuessGame.game.run
 }
