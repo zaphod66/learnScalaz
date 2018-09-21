6f248ecccacc66435bf5e6fa7ae8219f2f2b1437
Norbert Scheller <norbert@riskident.com>
2018-09-21 09:26:34 +0200

    master - added extension method safeContains as a reminder

---
+++ src/main/scala/com/myspace/stuff/TypeSafeContains.scala
@@ -0,0 +1,22 @@
+package com.myspace.stuff
+
+object TypeSafeContains extends App {
+
+  implicit class ListOps[A](as: List[A]) {
+    def safeContains(a: A): Boolean = as.contains(a)
+  }
+
+  val xs = List(1, 2, 3)
+  val r1 = xs.contains(2)
+  val r2 = xs.contains("Hello")
+
+  val r3 = xs.safeContains(2)
+//  val r4 = xs.safeContains("Hello")
+//  [error] /Users/nscheller/work/gitrepos/projects/zaphod66/learnScalaz/src/main/scala/com/myspace/stuff/TypeSafeContains.scala:14: type mismatch;
+//  [error]  found   : String("Hello")
+//  [error]  required: Int
+//  [error]   val r4 = xs.safeContains("Hello")
+//  [error]                            ^
+//  [error] one error found
+//  [error] (compile:compileIncremental) Compilation failed
+}
